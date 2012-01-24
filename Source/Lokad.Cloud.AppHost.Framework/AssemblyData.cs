a4b7731fd99530d3bb35c1ed62ccaedb09b4bf83
Christoph Ruegg <christoph.ruegg@lokad.com>
2012-01-24 10:00:07 +0100

    File header cosmetics

--- Source/Lokad.Cloud.AppHost.Framework/AssemblyData.cs
+++ Source/Lokad.Cloud.AppHost.Framework/AssemblyData.cs
@@ -1,4 +1,9 @@
-﻿namespace Lokad.Cloud.AppHost.Framework
+﻿#region Copyright (c) Lokad 2011-2012
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+
+namespace Lokad.Cloud.AppHost.Framework
 {
     public sealed class AssemblyData
     {
--- Source/Lokad.Cloud.AppHost.Framework/Definition/AssembliesHead.cs
+++ Source/Lokad.Cloud.AppHost.Framework/Definition/AssembliesHead.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿#region Copyright (c) Lokad 2011-2012
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+
+using System;
 using System.Runtime.Serialization;
 
 namespace Lokad.Cloud.AppHost.Framework.Definition
